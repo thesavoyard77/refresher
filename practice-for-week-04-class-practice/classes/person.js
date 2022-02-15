c1d17d57889f5172d3dbab31c1bf09bb7ce3a842
chris <felix.christopher.e@gmail.com>
2022-02-14 23:51:29 -0800

    classes are terrible syntax

--- practice-for-week-04-class-practice/classes/person.js
+++ practice-for-week-04-class-practice/classes/person.js
@@ -9,8 +9,18 @@ class Person {
     console.log(`Hi, I'm ${this.firstName} ${this.lastName}, and I'm ${this.age} years old.`)
   }
 
-  static introducePeople(people){
-    if (people )
+  static introducePeople(...people){
+    if (typeof people != Array) {
+      console.log("introducePeople only takes an array as an argument.")
+    } 
+
+    people.map(person => {
+      if (person instanceof Person) {
+        person.introduce()
+      } else {
+        console.log("All items in array must be Person class instances.")
+      }
+    })
   }
 }
 /****************************************************************************/
