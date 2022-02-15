8207dd560a2362ba615f1725f36dc804cd603308
chris <felix.christopher.e@gmail.com>
2022-02-14 23:36:22 -0800

    methods

--- practice-for-week-04-class-practice/classes/person.js
+++ practice-for-week-04-class-practice/classes/person.js
@@ -1,5 +1,18 @@
 // Your code here
+class Person {
+  constructor(firstName, lastName, age) {
+    this.firstName = firstName
+    this.lastName = lastName
+    this.age = age
+  }
+  introduce() {
+    console.log(`Hi, I'm ${this.firstName} ${this.lastName}, and I'm ${this.age} years old.`)
+  }
 
+  static introducePeople(people){
+    if (people )
+  }
+}
 /****************************************************************************/
 /******************* DO NOT EDIT CODE BELOW THIS LINE ***********************/
 
